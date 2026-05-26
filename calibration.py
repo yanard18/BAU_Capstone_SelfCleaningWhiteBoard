2c6f5218e4ec689afd73f088595e07225befe747
yanard <yanard18@gmail.com>
2026-05-26 21:17:04 +0300

    add: warp image

--- calibration.py
+++ calibration.py
@@ -6,9 +6,13 @@ import numpy.typing as npt  # type: ignore
 from typing import List, Tuple, Any
 
 
-def get_board_corners(image_path: str) -> npt.NDArray[np.float32]:
+def get_board_corners(img_pth: str) -> npt.NDArray[np.float32]:
     corner_points: List[Tuple[int, int]] = []
-    image = cv2.imread('mock3.png')
+    image = cv2.imread(img_pth)
+
+    if image is None:
+        raise FileNotFoundError(f"ERROR: OpenCV could not find the image at '{img_pth}'")
+
     clone = image.copy()
 
     def select_points(event: int, x: int, y: int,
@@ -40,3 +44,49 @@ def get_board_corners(image_path: str) -> npt.NDArray[np.float32]:
     cv2.destroyAllWindows()
 
     return np.array(corner_points, dtype=np.float32)
+
+
+def apply_homography(image: npt.NDArray[np.uint8],
+                     src_pts: npt.NDArray[np.float32]) -> npt.NDArray[np.uint8]:
+    """
+    Takes an image and the 4 clicked points, then returns the flattened 2D view.
+    """
+    rect = order_points(src_pts)
+    (tl, tr, br, bl) = rect
+
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    max_width = max(int(widthA), int(widthB))
+    
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    max_height = max(int(heightA), int(heightB))
+
+    dst_pts = np.array([
+        [0, 0],
+        [max_width - 1, 0],
+        [max_width - 1, max_height - 1],
+        [0, max_height - 1]
+    ], dtype=np.float32)
+
+    matrix = cv2.getPerspectiveTransform(rect, dst_pts)
+    warped_image = cv2.warpPerspective(image, matrix, (max_width, max_height))
+
+    return warped_image
+
+
+def order_points(pts: npt.NDArray[np.float32]) -> npt.NDArray[np.float32]:
+    """
+    Sorts the 4 points in this order: Top-Left, Top-Right, Bottom-Right, Bottom-Left.
+    """
+    rect = np.zeros((4, 2), dtype=np.float32)
+    
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+    
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+    
+    return rect
--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
 
+import cv2
 from calibration import get_board_corners
+from calibration import apply_homography
 
 
 if __name__ == "__main__":
-    arr = get_board_corners('mock3.png')
-    print(f"{arr}")
+    img_path = './mock_data/mock3.png'
+    src_pts = get_board_corners(img_path)
+    image = cv2.imread(img_path)
+    flat_board = apply_homography(image, src_pts)
+
+    cv2.namedWindow("Original", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("Original", 800, 600)
+    cv2.imshow("Original", image)
+
+    cv2.namedWindow("Flat", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("Flat", 800, 600)
+    cv2.imshow("Flat", flat_board)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
